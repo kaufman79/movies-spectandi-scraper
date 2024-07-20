655a3b1a8148921185fa48906718460b78a08563
kaufman79 <kaufman79@gmail.com>
2024-07-20 19:19:57 -0400

    convert to OOP paradigm

--- testing_hacker_news.py
+++ testing_hacker_news.py
@@ -2,23 +2,40 @@ import requests
 from bs4 import BeautifulSoup
 
 
-response = requests.get("https://news.ycombinator.com/news")
+class FirstArticle:
+    def __init__(self):
+        response = requests.get("https://news.ycombinator.com/news")
 
-yc_webpage = response.text
+        yc_webpage = response.text
 
-soup = BeautifulSoup(yc_webpage, "html.parser")
+        self.soup = BeautifulSoup(yc_webpage, "html.parser")
 
-# get first article's title
-article_one = soup.find(class_="titleline")
-title_and_link = article_one.find(name="a")
-article_title = title_and_link.get_text()
-print(article_title)
+        article_one = self.soup.find(class_="titleline")
+        self.title_and_link = article_one.find(name="a")
 
-# find article link
-article_link = title_and_link.get("href")
-print(article_link)
 
-# find article upvotes
-article_score = soup.find(class_="score")
-article_score = article_score.get_text()
-print(article_score)
+    def first_title(self):
+        """get first article's title"""
+        article_title = self.title_and_link.get_text()
+        return article_title
+
+
+    def first_link(self):
+        """find article link"""
+        article_link = self.title_and_link.get("href")
+        return article_link
+
+
+    def first_upvotes(self):
+        """find article upvotes"""
+        article_score = self.soup.find(class_="score")
+        article_score = article_score.get_text()
+        return article_score
+
+
+if __name__ == "__main__":
+    first_article = FirstArticle()
+
+    print(first_article.first_title())
+    print(first_article.first_link())
+    print(first_article.first_upvotes())

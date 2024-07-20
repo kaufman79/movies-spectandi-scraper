0ecefe874d7f251a0977ffbbe74fa3c81d310d15
kaufman79 <kaufman79@gmail.com>
2024-07-20 17:33:16 -0400

    get first articles name, link, and upvotes

--- testing_hacker_news.py
+++ testing_hacker_news.py
@@ -4,4 +4,21 @@ from bs4 import BeautifulSoup
 
 response = requests.get("https://news.ycombinator.com/news")
 
-print(response.text)
+yc_webpage = response.text
+
+soup = BeautifulSoup(yc_webpage, "html.parser")
+
+# get first article's title
+article_one = soup.find(class_="titleline")
+title_and_link = article_one.find(name="a")
+article_title = title_and_link.get_text()
+print(article_title)
+
+# find article link
+article_link = title_and_link.get("href")
+print(article_link)
+
+# find article upvotes
+article_score = soup.find(class_="score")
+article_score = article_score.get_text()
+print(article_score)

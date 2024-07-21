0082aeaeb28b181fc81c2e320c10dda4a6c7839d
kaufman79 <kaufman79@gmail.com>
2024-07-20 20:40:35 -0400

    finds most upvoted article and prints out title and link

--- testing_hacker_news.py
+++ testing_hacker_news.py
@@ -4,7 +4,7 @@ from bs4 import BeautifulSoup
 
 class ArticlesData:
     def __init__(self):
-        response = requests.get("https://news.ycombinator.com/news")
+        response = requests.get('https://news.ycombinator.com/news')
 
         yc_webpage = response.text
 
@@ -22,7 +22,6 @@ class ArticlesData:
             article_titles.append(article_title)
         return article_titles
 
-
     def links(self):
         """get article links"""
         article_links = []
@@ -31,16 +30,31 @@ class ArticlesData:
             article_links.append(article_link)
         return article_links
 
-
     def upvotes(self):
         """get article upvotes"""
         article_scores = [score.get_text() for score in self.soup.find_all(class_="score")]
-        return article_scores
+        int_scores = [int(score.split()[0]) for score in article_scores]
+        return int_scores
 
 
 if __name__ == "__main__":
     data = ArticlesData()
 
-    print(data.titles())
-    print(data.links())
-    print(data.upvotes())
+    # print(data.titles())
+    # print(data.links())
+    # print(data.upvotes())
+
+    titles = data.titles()
+    links = data.links()
+    upvotes = data.upvotes()
+
+    # find index of most upvoted article
+    max_score = max(upvotes)
+    max_index = upvotes.index(max_score)
+
+    # find title & link of most upvoted article
+    max_title = titles[max_index]
+    max_link = links[max_index]
+
+    print(max_title)
+    print(max_link)

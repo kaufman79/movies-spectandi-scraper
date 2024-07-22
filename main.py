a6bed07060c9916b97aca98547188deb026afb52
kaufman79 <kaufman79@gmail.com>
2024-07-22 09:38:58 -0400

    \n no longer inserted into list, but when making txt file

--- main.py
+++ main.py
@@ -7,9 +7,10 @@ soup = BeautifulSoup(site, "html.parser")
 
 movies_list = soup.find_all(class_="listicleItem_listicle-item__title__BfenH")
 
-movies_texts = [movie.get_text() + "\n" for movie in movies_list]
+movies_texts = [movie.get_text() for movie in movies_list]
 
 movies_texts.reverse()
 
 with open("movies.txt", 'w') as file_local:
-    file_local.writelines(movies_texts)
+    for movie in movies_texts:
+        file_local.write(f"{movie}\n")
